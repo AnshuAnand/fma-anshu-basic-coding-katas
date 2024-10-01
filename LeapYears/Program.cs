212de9f32d1bac87d2c79e1cde5cac676ec39745
Anshu Anand <anshu.anand@myob.com>
2024-10-02 09:48:21 +1000

    Add the logic for leap year

--- LeapYears/Program.cs
+++ LeapYears/Program.cs
@@ -1,9 +1,55 @@
-﻿namespace LeapYears;
+﻿using System.Xml;
+
+namespace LeapYears;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        //Find the year of current date
+
+        DateTime thisDay = DateTime.Today;
+        string currentDate = thisDay.ToString("d");
+        Console.WriteLine("Current Date in dd/mm/yyyy format is: " + currentDate);
+        int currentDateLength = currentDate.Length;
+        string currentYearAsString = currentDate.Substring(currentDateLength - 4);
+        Console.WriteLine("Current year is: " + currentYearAsString);
+
+        //Logic to check if current year is a leap year
+
+        //int currentYear = Int32.Parse(currentYearString);
+        int currentYear = 2025; //Non-leap year as starting value
+        int remainder = currentYear % 4;
+        string startYear;
+        string output;
+
+        //Check if current year is a leap year
+        if (remainder == 0)
+        {
+            startYear = currentYear.ToString();
+            output = startYear;
+            for (int i = 1; i < 4; i++)
+            {
+                int nextLeapYear = Convert.ToInt32(startYear) + 4;
+                string nextLeapYearAsString = Convert.ToString(nextLeapYear);
+                output = output + " " + nextLeapYearAsString;
+                Console.WriteLine("Output is: " + output);
+                startYear = nextLeapYearAsString;
+            }
+        }
+        else
+        {
+            startYear = (currentYear + (4 - remainder)).ToString();
+            output = startYear;
+            for (int i = 1; i < 4; i++)
+            {
+                int nextLeapYear = Convert.ToInt32(startYear) + 4;
+                string nextLeapYearAsString = Convert.ToString(nextLeapYear);
+                output = output + " " + nextLeapYearAsString;
+                Console.WriteLine("Output is: " + output);
+                startYear = nextLeapYearAsString;
+            }
+        }
     }
 }
